9722b45cde83065bf452e2209a0883ee7cfcee3f
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-22 08:58:28 -0600

    commit tree 299c39747e15113b845078e47fdb17666cb694c1

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,26 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods 
+  extend Findable 
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -33,10 +29,6 @@ class Artist
   end
 
   def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+    songs.each { |song| add_song(song)}
   end
 end
